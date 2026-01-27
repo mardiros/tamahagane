bcf01069a420cc30c940862f3de6995763a77e6d
Guillaume Gauvrit <guillaume@gauvr.it>
2026-01-27 07:40:14 +0100

    Refactor tests

--- tests/unittests/test_scanner.py
+++ tests/unittests/test_scanner.py
@@ -1,35 +1,51 @@
+from collections.abc import Sequence
+
+import pytest
+
 import tamahagane as th
 from tests.lazyapp import Cli, Registries
 
 
-def test_scanner_ignore_package():
+@pytest.mark.parametrize(
+    "scanmod,ignore,expected",
+    [
+        pytest.param(
+            ["tests.lazyapp.commands"],
+            ["tests.lazyapp.commands.ignoreme"],
+            {"hello-sub", "hello-world"},
+            id="regular package / exclude packge",
+        ),
+        pytest.param(
+            ["tests.lazyapp.commands"],
+            ["tests.lazyapp.commands.ignoreme.wanted"],
+            {"hello-sub", "hello-world"},
+            id="regular package / exclude module",
+        ),
+        pytest.param(
+            ["tests.lazyapp.commands"],
+            ["tests.lazyapp.commands"],
+            set[str](),
+            id="ignore all",
+        ),
+        pytest.param(
+            ["tests.lazyapp.commands"],
+            [".ignoreme"],
+            {"hello-sub", "hello-world"},
+            id="ignore relative",
+        ),
+        pytest.param(
+            ["tests.lazyapp.commands"],
+            [],
+            {"hello-sub", "hello-world", "hello-wanted"},
+            id="all",
+        ),
+    ],
+)
+def test_scanner_ignore(scanmod: list[str], ignore: Sequence[str], expected: set[str]):
     cli = Cli()
     scanner = th.Scanner[Registries](Registries(cli=cli))
-    scanner.scan("tests.lazyapp.commands", ignore=["tests.lazyapp.commands.ignoreme"])
-    assert set(cli.commands.keys()) == {"hello-sub", "hello-world"}
-
-
-def test_scanner_ignore_module():
-    cli = Cli()
-    scanner = th.Scanner[Registries](Registries(cli=cli))
-    scanner.scan(
-        "tests.lazyapp.commands", ignore=["tests.lazyapp.commands.ignoreme.wanted"]
-    )
-    assert set(cli.commands.keys()) == {"hello-sub", "hello-world"}
-
-
-def test_scanner_ignore_all():
-    cli = Cli()
-    scanner = th.Scanner[Registries](Registries(cli=cli))
-    scanner.scan("tests.lazyapp.commands", ignore=["tests.lazyapp.commands"])
-    assert set(cli.commands.keys()) == set()
-
-
-def test_scanner_ignore_relative():
-    cli = Cli()
-    scanner = th.Scanner[Registries](Registries(cli=cli))
-    scanner.scan("tests.lazyapp.commands", ignore=[".ignoreme"])
-    assert set(cli.commands.keys()) == {"hello-sub", "hello-world"}
+    scanner.scan(*scanmod, ignore=ignore)
+    assert set(cli.commands.keys()) == expected
 
 
 def test_scan_twice():
@@ -39,13 +55,3 @@ def test_scan_twice():
     assert set(cli.commands.keys()) == {"hello-sub", "hello-world"}
     scanner.scan("tests.lazyapp.commands")
     assert set(cli.commands.keys()) == {"hello-sub", "hello-world", "hello-wanted"}
-
-
-def test_scanner_all():
-    cli = Cli()
-    scanner = th.Scanner[Registries](Registries(cli=cli))
-    scanner.scan("tests.lazyapp.commands")
-    assert set(cli.commands.keys()) == {"hello-sub", "hello-world", "hello-wanted"}
-    assert cli.commands["hello-world"]("hello") == "Hello World"
-    assert cli.commands["hello-sub"]("hello") == "Hello Sub"
-    assert cli.commands["hello-wanted"]("hello") == "Hello Wanted"
